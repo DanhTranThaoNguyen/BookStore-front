35ef0df4a10f143e81bf06773deb0d927db5d107
DanhTranThaoNguyen <danhnguyen.st03@gmail.com>
2023-05-02 12:08:58 +0700

    Khoi tao du an

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+    <h1>Hello, Vue.js!</h1>
+  </header>
+  <main>
+    
+  </main>
+</template>
+
+<style>
+.page {
+max-width: 400px;
+margin: auto;
+}
+</style>
